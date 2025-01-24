0e6a7e508901fe594ccb70544b6be2b668c65c9d
AvinashRajput12 <rajputboy945456@gmail.com>
2025-01-24 12:18:23 +0530

    RoleBaseRedirectAdded

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLL_CHOICE = [
+        ('user','User'),
+        ('buyer','Buyer'),
+        ('vendor','Vendor')
+    ]
+    role = models.CharField(max_length=15, choices=ROLL_CHOICE, default=False)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+    def is_user(self):
+        return self.role == 'user'
+    
+    def is_buyer(self):
+        return self.role == 'buyer'
+    
+
+    def is_vendor(self):
+        return self.role == 'vendor'
+
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import CustomLoginView, UserRegistrationView, VendorDashboardView, BuyerDashboardView, no_permission , UserDashboardView
 
 
 urlpatterns = [
-     
+     path('login/', CustomLoginView.as_view(), name='login'),
+     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('vendor/dashboard/', VendorDashboardView.as_view(), name='vendor_dashboard') ,
+    path('buyer/dashboard/', BuyerDashboardView.as_view(), name='buyer_dashboard') ,
+    path('user/dashboard/', UserDashboardView.as_view(), name='vendor_dashboard') ,
+    path('no_permission/', no_permission, name='no_permission') ,
 ]
